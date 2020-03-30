4af4ac1d161c011605dfbc8e833b2a19ee487174
Vishesh Singh <VisheshSingh16@gmail.com>
2020-03-30 18:56:26 -0500

    Convert req param to middleware 🤗

--- routes/articles.js
+++ routes/articles.js
@@ -10,14 +10,16 @@ router.get('/', (req, res) => {
     .catch(err => res.status(400).json({ message: err.message }));
 });
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  console.timeLog(id);
-  Articles.findById(id)
-    .then(article => {
-      return res.json(article);
-    })
-    .catch(err => res.status(400).json({ message: err.message }));
+router.get('/:id', getArticle, (req, res) => {
+  //    METHOD 1
+  //   Articles.findById(id)
+  //     .then(article => {
+  //       return res.json(article);
+  //     })
+  //     .catch(err => res.status(400).json({ message: err.message }));
+
+  // METHOD 2
+  res.json(res.article);
 });
 
 router.post('/add', (req, res) => {
@@ -30,34 +32,85 @@ router.post('/add', (req, res) => {
 
   newArticle
     .save()
-    .then(() => res.status(201).json({ message: newArticle }))
+    .then(() => res.status(201).json(newArticle))
     .catch(err => res.status(400).json({ message: err.message }));
 });
 
-router.patch('/:id', (req, res) => {
+router.patch('/:id', getArticle, async (req, res) => {
   const { title, description, author } = req.body;
-  Articles.findById(req.params.id)
-    .then(article => {
-      article.title = title || article.title;
-      article.description = description || article.description;
-      article.author = author || article.author;
-      article
-        .save()
-        .then(() => res.status(200).json({ message: article }))
-        .catch(err => res.status(400).json({ message: err.message }));
-    })
-    .catch(err =>
-      res.status(400).json({ message: `Could not find article with that id` })
-    );
+  // METHOD 1
+  //   Articles.findById(req.params.id)
+  //     .then(article => {
+  //       article.title = title || article.title;
+  //       article.description = description || article.description;
+  //       article.author = author || article.author;
+  //       article
+  //         .save()
+  //         .then(() => res.status(200).json({ message: article }))
+  //         .catch(err => res.status(400).json({ message: err.message }));
+  //     })
+  //     .catch(err =>
+  //       res.status(400).json({ message: `Could not find article with that id` })
+  //     );
+
+  // METHOD 2
+  try {
+    // console.log();
+    // if (title !== null) {
+    //   res.article.title = title;
+    // }
+    // if (description !== null) {
+    //   res.article.description = description;
+    // }
+    // if (author !== null) {
+    //   res.article.author = author;
+    // }
+    title
+      ? (res.article.title = title)
+      : (res.article.title = res.article.title);
+    description
+      ? (res.article.description = description)
+      : (res.article.description = res.article.description);
+    author
+      ? (res.article.author = author)
+      : (res.article.author = res.article.author);
+    const updatedArticle = await res.article.save();
+    res.status(200).json(updatedArticle);
+  } catch (err) {
+    res.status(500).json({ message: 'COULD NOT UPDATE!' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  Articles.findByIdAndDelete(id)
-    .then(() => res.status(200).json({ message: 'DELETED!' }))
-    .catch(err =>
-      res.status(400).json({ message: `Could not delete article with that id` })
-    );
+router.delete('/:id', getArticle, async (req, res) => {
+  // METHOD 1
+  //   const { id } = req.params;
+  //   Articles.findByIdAndDelete(id)
+  //     .then(() => res.status(200).json({ message: 'DELETED!' }))
+  //     .catch(err =>
+  //       res.status(400).json({ message: `Could not delete article with that id` })
+  //     );
+
+  // METHOD 2
+  try {
+    await res.article.remove();
+    res.status(200).json({ message: 'DELETED!' });
+  } catch (err) {
+    res.status(500).json({ message: 'COULD NOT DELETE!' });
+  }
 });
+
+async function getArticle(req, res, next) {
+  let article;
+  try {
+    article = await Articles.findById(req.params.id);
+    if (!article) {
+      return res.status(400).json({ message: 'Cannot find article' });
+    }
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
+  }
+  res.article = article;
+  next();
+}
 
 module.exports = router;
