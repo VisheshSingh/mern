c6c3eaa3542368a6ea56d772e9e1b377a5985113
Vishesh Singh <VisheshSingh16@gmail.com>
2020-03-30 16:26:22 -0500

    Initial setup; connecting to DB 🏃‍♂️💼

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('express');
+
+require('dotenv').config(); // BRING IN THE ENV VARIABLES 🖖
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 8080;
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => console.log(`MONGODB successfully connected 😎`));
+
+app.listen(port, () => console.log(`App is running on PORT: ${port}...`));
